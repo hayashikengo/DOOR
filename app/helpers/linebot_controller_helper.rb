1c1207412e29b33885336030c533bd7e5892a4e7
“kenghaya” <kenghaya@yahoo-corp.jp>
2018-09-18 10:50:07 +0900

    add LinebotControllerController

---
+++ app/helpers/linebot_controller_helper.rb
@@ -0,0 +1,2 @@
+module LinebotControllerHelper
+end
---
+++ test/controllers/linebot_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LinebotControllerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
