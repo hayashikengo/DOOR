33d07814e179b2a4837568c449dbbc0b0b443c0f
“hkengo” <hayashikengojp@gmail.com>
2018-09-24 03:11:08 +0900

    change db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_23_114431) do
+ActiveRecord::Schema.define(version: 2018_09_23_180850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2018_09_23_114431) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "line_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "text"
+    t.datetime "read_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_clovas", force: :cascade do |t|
