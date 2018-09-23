d399920b1a3c2e97e36ba8f75b4b19016a97722a
“kenghaya” <kenghaya@yahoo-corp.jp>
2018-09-23 17:10:26 +0900

    modified db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_23_045458) do
+ActiveRecord::Schema.define(version: 2018_09_23_080802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clovas", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
