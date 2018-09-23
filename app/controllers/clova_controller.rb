a3ce137dbf1c1f4a036c53587cd38241295cdfdd
“hkengo” <hayashikengojp@gmail.com>
2018-09-23 21:26:40 +0900

    fix

--- app/controllers/clova_controller.rb
+++ app/controllers/clova_controller.rb
@@ -1,5 +1,14 @@
 class ClovaController < ApplicationController
   def callback
+    # message = {
+    #   type: 'text',
+    #   text: 'hello'
+    # }
+    # # line_user_id =
+    # response = client.push_message("userId", message)
+    # p response
+    p session.to_hash.to_s
+
 
     render 'clova/callback', formats: 'json', handlers: 'jbuilder'
   end
