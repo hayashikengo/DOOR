f75169178ec7a0b8cee50a70104c4b164e4742de
“hkengo” <hayashikengojp@gmail.com>
2018-09-24 18:39:59 +0900

    デザイン修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :clovas
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  # root 'welcome#index'
+  root 'users#index'
   post '/linebot/callback' => 'linebot#callback'
   post '/clova/callback' => 'clova#callback'
 end
