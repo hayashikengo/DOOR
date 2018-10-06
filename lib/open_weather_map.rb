87be02af554cbaa6befd3da8c5fd5dbba8d24ab3
“hkengo” <hayashikengojp@gmail.com>
2018-10-06 18:43:52 +0900

    add OpenWeatherMap

---
+++ lib/open_weather_map.rb
@@ -0,0 +1,14 @@
+class OpenWeatherMap
+  require "json"
+  require "open-uri"
+  
+  BASE_URL = "https://samples.openweathermap.org/data/2.5/forecast".freeze
+  TOKYO_CITY_ID = '1850147'
+
+  # tokyo: key='id', val='1850147'
+  # akashi: key='q', val='Akashi'
+  def self.get(key = 'id', val = TOKYO_CITY_ID)
+    json_response = open(BASE_URL + "?#{key}=#{val}&APPID=#{ENV['Open_Weather_Map_API_KEY']}")
+    JSON.parse(json_response.read)
+  end
+end
