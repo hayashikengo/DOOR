3b395700aa798e0c816102f8ae0f0617068d2366
“hkengo” <hayashikengojp@gmail.com>
2018-10-08 19:38:41 +0900

    debug

--- app/controllers/clova_controller.rb
+++ app/controllers/clova_controller.rb
@@ -4,12 +4,11 @@ class ClovaController < ApplicationController
   before_action :set_shouldEndSession, only: [:callback]
 
   def callback
-    @voice_message = nil
+    @voice_message = "おかえりなさい！"
 
-    pp @shouldEndSession
     if @shouldEndSession
       # ラインボットへ通知を送る
-      send_push_message(to_user)
+      send_push_message
 
       # @voice_messageの設定
       set_voice_message
@@ -22,28 +21,19 @@ class ClovaController < ApplicationController
 
   def set_shouldEndSession
     slots = params['request']['intent']['slots']
-    pp "*" * 100
-    pp params['request']['intent']
-    pp params['request']['intent'].class
-    pp params[:request][:intent][:slots]
-    pp params.permit!.to_h[:request][:intent][:slots]
-    pp params.permit!.to_h[:request][:intent]
-    pp "*" * 100
-    pp slots.nil? ? false : true
-
     @shouldEndSession = slots.nil? ? false : true
   end
 
   def set_voice_message
-    if to_user = @clova.user
+    unless @to_user
       @voice_message = "LINEBotのDOOR(Perent)で伝言板に追加してください。"
       return
     end
-    
+
     # クローバスキルが呼び出された時に返す返答
-    if messages = to_user.messages.presence
-      @voice_message = "#{to_user.displayName}からの連絡です。。。"
-      to_user.messages.should_send.each.with_index(1) do |message, i|
+    if messages = @to_user.messages.presence
+      @voice_message = "#{@to_user.displayName}からの連絡です。。。"
+      @to_user.messages.should_send.each.with_index(1) do |message, i|
         # @voice_message += "#{i}件目です。"
         @voice_message += message.text + '。'
       end
@@ -53,18 +43,19 @@ class ClovaController < ApplicationController
   end
 
   # DOOR(Parent)へのプッシュ通知
-  def send_push_message(to_user)
-    text_message = "#{to_user.displayName}さんからの伝言をお伝えしました！"
+  def send_push_message
+    text_message = "#{@to_user.displayName}さんからの伝言をお伝えしました！"
     message = {
       type: 'text',
       text: text_message
     }
-    response = client.push_message(to_user.line_user_id, message)
+    response = client.push_message(@to_user.line_user_id, message)
     p response
   end
 
   def set_clova
     line_user_id = params['session']['user']['userId']
     @clova = Clova.find_and_set_line_user_id(line_user_id)
+    @to_user = @clova.user
   end
 end
